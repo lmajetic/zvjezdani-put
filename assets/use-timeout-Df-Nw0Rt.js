c9e7908107f209d91889677fdd385041c15306b4
lmajetic <lukamajeticc@gmail.com>
2026-05-24 19:53:04 +0200

    Updates

---
+++ assets/use-timeout-Df-Nw0Rt.js
@@ -0,0 +1,1 @@
+import{L as e,N as t,R as n}from"./index-D7nfLams.js";import{o as r}from"./QBtn-DRjSaWpK.js";function i(){let i=null,a=t();function o(){i!==null&&(clearTimeout(i),i=null)}return n(o),e(o),{removeTimeout:o,registerTimeout(e,t){o(),r(a)===!1&&(i=setTimeout(()=>{i=null,e()},t))}}}export{i as t};
